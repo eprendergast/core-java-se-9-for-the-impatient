37e991ebfc6ace62c7a972c15ef69e836c27fb3a
Elizabeth Prendergast <elizabeth.prendergast@yapily.com>
2020-02-15 18:17:11 +0000

    completed ch01 q08

--- src/main/java/ch01/q08/Substrings.java
+++ src/main/java/ch01/q08/Substrings.java
@@ -1,8 +1,22 @@
 package ch01.q08;
 
-
-// read a string; print all non empty substrings
+import java.util.Scanner;
 
 class Substrings {
     
+    public static void main (String[] args) {
+        System.out.print("Enter a string: ");
+        final Scanner in = new Scanner(System.in);
+        String string = in.nextLine();
+        printSubstrings(string);
+    }
+
+    public static void printSubstrings(String string) {
+        for (int i = 0; i < string.length(); i ++) {
+            for (int j = i + 1; j <= string.length(); j++) {
+                System.out.println(string.substring(i, j));
+            }
+        }
+    }
 }
+
