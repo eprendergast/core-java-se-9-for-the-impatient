8ec5e02bb49b9f1ee9cdea387bd673165be70440
Elizabeth Prendergast <elizabeth.prendergast@yapily.com>
2020-02-16 16:32:32 +0000

    finished LotteryNumbers

--- src/main/java/ch01/q13/LotteryNumbers.java
+++ src/main/java/ch01/q13/LotteryNumbers.java
@@ -1,1 +1,27 @@
-// another test
+package ch01.q13;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+class LotteryNumbers {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> winningNumbers = pickDistinctNumbers();
+        System.out.println(winningNumbers);
+    }
+
+    public static ArrayList<Integer> pickDistinctNumbers() {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 0; i < 49; i ++) {
+            numbers.add(i + 1);
+        }
+        Random r = new Random();
+        ArrayList<Integer> winningNumbers = new ArrayList<Integer>();
+        for (int i = 0; i < 6; i++) {
+            int index = r.nextInt(numbers.size());
+            winningNumbers.add(i, numbers.get(index));
+            numbers.remove(index);
+        }
+        return winningNumbers;
+    }
+}
